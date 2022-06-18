4f83bb37ddb945b8a18cbee6c2160333f4a35ae5
JoelOgira-2020 <joelogira@gmail.com>
2022-06-18 23:38:40 +0300

    Created the Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">      
       <Header />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer text-center py-2 bg-dark text-chocolate">
+        <p className='pt-2'>Hooks & State Coop &copy; 2022</p>
+    </div>
+  )
+}
+
+export default Footer;
